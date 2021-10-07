b8280273a273c17f6e67a8a7a68e8e7beb93eaf0
Sohta0318 <nicosohta0318@gmail.com>
2021-10-07 16:11:24 -0700

    initial

---
+++ classes.java
@@ -0,0 +1,15 @@
+public class classes {
+  public static void main(String[] args) {
+    String name = "Hello";
+    switch(name){
+      case "hello": 
+      System.out.println("correct");
+      break;
+      case "Hello": 
+      System.out.println("correct2");
+      break;
+
+    }
+    
+  }
+}
